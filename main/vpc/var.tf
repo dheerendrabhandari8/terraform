ec0b35dbe26527f54a4872df488fc8790029fe1b
Ubuntu <ubuntu@ip-172-31-2-200.ap-south-1.compute.internal>
2025-03-11 06:46:15 +0000

    second commit for vpc and subnet

--- main/vpc/var.tf
+++ main/vpc/var.tf
@@ -1,16 +1,8 @@
-
 variable "cidr_block"{
-#default = ""
-description = "The CIDR block for the VPC"
 type = string
-
-#cidr_block = var.cidr_block
 }
 
 variable "Name"{
 type = string
-
-#default = "bhandari"
-
 }
 
